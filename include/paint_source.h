51da6ffdd6ce59f89acceb85cf17240d9d705ffa
Martin Klima <martin@argenite.org>
2024-03-10 22:25:36 +0100

    Update description of the paint mode

--- include/paint_source.h
+++ include/paint_source.h
@@ -3,43 +3,17 @@
 
 
 /**
-Modes:
- - Edit keyframe
- - Play
+The original idea was that the players/users will be using a gamepad to paint on the LED chain. There would be buttons to change
+colour of the brush, and size of the brush and transparency and what not, and set state of the brush as an animation keyframe and
+then create another one... The remnants of that idea can still be seen around, I did not have time to properly extirpate them all. 
 
- Controls:
-	
-	== Edit ==
+Fortunately, I told about that to my wife and she said she couldn't be bothered to learn sucha a complicated control scheme and so
+I wrote a control panel in JavaScript and it is part of the other repostitory, LED_programs. So everything this source does is to 
+listen to the message from the Python server and then updates all the LEDs. The only thing it does on its own is animation, but we
+have no fancy keyframes and such, we can just run the pattern along the chain and/or change the lightness slighlty.
 
-		Move brush			Left stick
-		Hue					R1/L1
-		Saturation			Right stick left/right
-		Lightness			Right stick up/down
-		Opacity				D-pad up/down
-		Brush size			R2/L2
-		Save keyframes		Y
-		Apply				A
-		Undo				B
-		Redo				X
-		Change keyframe		D-pad left/right
-		Add keyframe		L3
-		Delete keyframe		R3
-		Enter Play mode		Start
-
-	== Play ==
-
-		Keyframe time
-		Speed along chain
-		Start/Stop moving along the chain
-		Easing function between keyframes
-		Load keyframes
-
- Display behaviour
-
-	== Edit ==
-
-	* Brush keeps blinking on/off
-
+Originally, I intended this to be really just a painting application, no games, no secret messages. In the end it ended like it
+always does.
  */
 
 
